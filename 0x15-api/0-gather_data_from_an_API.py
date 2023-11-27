7768534c73a2c6b01f67816824379762ae049695
Dillys Annan <dillysannan@gmail.com>
2023-11-27 13:05:45 -0800

    get data from a rest api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,14 @@ if __name__ == "__main__":
     emp_id = sys.argv[1]
     resp = requests.get(url + "users/{}".format(emp_id))
     user = resp.json()
-    params = {"userId" : emp_id}
+    params = {"userId": emp_id}
     todo_resp = requests.get(url + "todos", params=params)
     todos = todo_resp.json()
     completed = []
     for todo in todos:
-        if todo.get("completed") == True:
+        if todo.get("completed") is True:
             completed.append(todo.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),len(completed),len(todos)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
     for todo in completed:
         print("\t {}".format(todo))

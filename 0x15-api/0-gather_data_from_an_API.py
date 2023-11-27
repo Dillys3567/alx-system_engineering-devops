769438684741ff68bf4fe1759dc6a55ee46cf248
Dillys Annan <dillysannan@gmail.com>
2023-11-27 13:02:09 -0800

    get data from a rest api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Gather data for an employee from a REST ap
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    emp_id = sys.argv[1]
+    resp = requests.get(url + "users/{}".format(emp_id))
+    user = resp.json()
+    params = {"userId" : emp_id}
+    todo_resp = requests.get(url + "todos", params=params)
+    todos = todo_resp.json()
+    completed = []
+    for todo in todos:
+        if todo.get("completed") == True:
+            completed.append(todo.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),len(completed),len(todos)))
+    for todo in completed:
+        print("\t {}".format(todo))

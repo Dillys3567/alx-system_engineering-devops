b93e91f86d9f0c0b671e77bbcd4628da0606ab19
Dillys Annan <dillysannan@gmail.com>
2023-12-13 05:21:40 -0800

    files

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""queries the Reddit API
+    parses the title of all hot articles
+    and prints a sorted count of given keywords
+"""
+import json
+import requests
+import sys
+
+def count_words(subreddit, word_list):
